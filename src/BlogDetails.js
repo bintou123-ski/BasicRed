1b42c6a68ace1cfb7f044227cda75f8baa114f61
Bintou Sanogo <bintou123-ski@github.com>
2021-07-31 23:59:01 +0800

    Reusiing Custom Hooks

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 
 const BlogDetails = () => {
     const {id}= useParams();
+    const{data: blog ,loading,error} = useFetch('http://localhost:8000/blogs/' + id)
     return ( 
           <div className='blog-details'>
-              <h2>BlogDetails - {id}</h2>
+              {loading && <div>Loading....</div>}
+              {error  && <div>{error}</div>}
+              {blog && (
+                  <article>
+                      <h2>{blog.title}</h2>
+                      <p>Written by {blog.author}</p>
+                      <p>{blog.body}</p>
+                  </article>
+              )}
           </div>
     );
 }
--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,18 @@
+import React,{useState} from "react";
 const Create = () => {
+    const [body, setBody] = useState('')
     return ( 
-        <div>Add a new Page</div>
+        <div>
+            <form>
+                <label>Blog body:</label>
+                <textarea
+                required
+                value={body}
+                onChange={(e)=>setBody(e.target.value)}
+                ></textarea>
+            </form>
+            <button>Add blog</button>
+        </div>
      );
 }
  
